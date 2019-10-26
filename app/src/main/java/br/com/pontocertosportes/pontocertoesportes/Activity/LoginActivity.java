725075058d46dcd26f7e9f5410c059c8b522b7cf
Brendhow <brendhowpereira@hotmail.com>
2019-10-26 16:04:33 -0300

    Remove comments in login

--- app/src/main/java/br/com/pontocertosportes/pontocertoesportes/Activity/LoginActivity.java
+++ app/src/main/java/br/com/pontocertosportes/pontocertoesportes/Activity/LoginActivity.java
@@ -29,7 +29,6 @@ public class LoginActivity extends AppCompatActivity {
         if(user.getText().toString().equals("user") && password.getText().toString().equals("password")){
             Intent openHomeActivity = new Intent(this, HomeActivity.class);
             startActivity(openHomeActivity);
-            //nameUserTextView.setText(nameUsuario);
         }else{
             Toast toast = Toast.makeText(getApplicationContext(),"Usuário ou senha incorretos",
                     Toast.LENGTH_SHORT);
@@ -37,26 +36,3 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
  }
-
-/*
-    tenho 0.0028 bit coins
-    trei 0.332 bit coins*?
-*/
-
-/*
-<EditText
-    android:layout_width="wrap_content"
-    android:layout_height="match_parent"
-    android:ems="9"
-    android:fontFamily="@font/allerstd"
-    android:hint="Usuário"
-    android:inputType="textEmailAddress"
-    android:maxLines="1"
-    android:minLines="1"
-    android:textColor="@android:color/white"
-    android:textColorHint="@android:color/darker_gray"
-    android:textSize="16dp"
-    tools:layout_editor_absoluteX="77dp"
-    tools:layout_editor_absoluteY="118dp" />
-
-     */
